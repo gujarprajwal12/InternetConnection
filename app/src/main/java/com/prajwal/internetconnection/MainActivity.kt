7ff291f03a1be360274e202e401065d4cd3d9d7a
prajwal.gujar <prajwal.gujar@barquecontech.com>
2022-06-23 16:44:51 +0530

    update code

--- app/src/main/java/com/prajwal/internetconnection/MainActivity.kt
+++ app/src/main/java/com/prajwal/internetconnection/MainActivity.kt
@@ -2,14 +2,14 @@ package com.prajwal.internetconnection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import com.prajwal.internetconnectivitylibrary.Internetcheck
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+      
     }
 
 }
